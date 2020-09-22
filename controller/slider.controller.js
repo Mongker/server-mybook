20e5fd6535538343932f3b18fc892fa0c37acdf3
MaiDao99 <68932625+MaiDao99@users.noreply.github.com>
2020-09-23 00:29:24 +0700

    MongLV: fix

--- controller/slider.controller.js
+++ controller/slider.controller.js
@@ -10,21 +10,21 @@ module.exports = {
                 const objectData = {};
                 data.map((item)=> {
                     objectData[item._id] = item;
-                })
+                });
                 console.log(objectData);
                 return res.status(200).json(objectData)
-            };
+            }
         })
     },
     POST:async function (req, res) {
         console.log(req.body);
         // req.body.created = now;
-       const product = await Slider(req.body).save().then((product) => { res.json({ message: 'successfully' }) }).catch((err) => { res.status(500).json({ message: 'error' }) })
+       await Slider(req.body).save().then((product) => { res.json({ message: 'SUCCESS' }) }).catch((err) => { res.status(500).json({ message: 'error' }) })
     },
     DELETE:async function (req, res) {
         await Slider.findByIdAndRemove({_id: req.params.id}, function(err, Product){
             if(err) res.json(err);
-            else res.json({ message: 'Successfully removed'});
+            else res.json({ message: 'SUCCESS'});
         });
     },
     UPDATE:async function (req, res) {
@@ -32,12 +32,13 @@ module.exports = {
             if (!Slider)
                 res.status(404).send("data is not found");
             else {
+                console.log('update: '+req);
                 Slider.name = req.body.name;
                 Slider.image_link = req.body.image_link;
                 Slider.index = req.body.index;
                 console.log(Slider);
                 Slider.save().then(business => {
-                    res.json({ message: 'Successfully update'});
+                    res.json({ message: 'SUCCESS'});
                 })
                     .catch(err => {
                         res.status(400).send({message: 'Failed to update Product'});
